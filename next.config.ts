2b21869788b6cea2e8880006984088d445f4b160
oms <po1202222@gmail.com>
2025-01-13 13:27:57 +0900

    [feat][오명섭] : 프로젝트 초기 세팅

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'images.unsplash.com',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
+  typescript: {
+    ignoreBuildErrors: true, // TypeScript 오류를 무시하고 빌드 진행
+  },
+};
+
+export default nextConfig;
