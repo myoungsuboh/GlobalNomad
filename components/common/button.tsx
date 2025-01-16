62b230c1f89e3bfcc195b26f6b8c1fb17b2c8f89
notebook <po1202222@gmail.com>
2025-01-16 20:57:12 +0900

    [feat] : button disabled 기능 추가

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -5,12 +5,13 @@ interface ButtonType {
   className?: string;
   type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
+  disabled?: boolean;
   children?: React.ReactNode;
 }
 
-export default function Button({className = '', type = 'button', children, onClick}: ButtonType) {
+export default function Button({className = '', type = 'button', disabled = false, children, onClick}: ButtonType) {
   return (
-    <button className={className} type={type} onClick={onClick}>
+    <button className={className} disabled={disabled} type={type} onClick={onClick}>
       {children}
     </button>
   );
